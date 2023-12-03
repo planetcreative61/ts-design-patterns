9fd32e28f28253fb214dbf21cb45f109cc2bfddd
planetcreative61 creative <planetcreative61@gmail.com>
2023-12-03 19:56:41 +0000

    added builder

--- builder/dir-scrapper-class.ts
+++ builder/dir-scrapper-class.ts
@@ -1,3 +1,49 @@
+import fs from "fs";
 interface IFileReader {
-
+    isJSONFile(file: string): boolean;
+    readText(file: string): string;
+    readJSON(file: string): unknown;
 }
+
+class DirectoryScrapper implements IFileReader {
+    constructor(public dirPath: string, public FileReader: IFileReader) {}
+    scanFiles() {
+        return fs.readdirSync(this.dirPath).reduce<Record<string, unknown>>((acc: Record<string, unknown>, file: string) => {
+            if(this.isJSONFile(file)) {
+                acc[file] = this.FileReader.readJSON(`${this.dirPath + "/" + file}`);
+            } else {
+                acc[file] = this.FileReader.readText(`${this.dirPath + "/" + file}`);
+            }
+            return acc;
+        }, {});
+    }
+    isJSONFile(file: string): boolean {
+        return false
+    }
+    readText(file: string): string {
+        return ""
+    }
+    readJSON(file: string): unknown {
+        return ""
+    }
+}
+
+
+class FileReader implements IFileReader {
+    isJSONFile(file: string): boolean {
+        return file.endsWith(".json");
+    }
+    readText(file: string): string {
+        return fs.readFileSync(file, "utf8").toString();
+    }
+    readJSON(file: string): unknown {
+        return JSON.parse(fs.readFileSync(file, "utf8").toString())
+    }
+}
+
+const fileReader = new FileReader();
+const directoryScrapper = new DirectoryScrapper("./data", fileReader);
+
+const output = directoryScrapper.scanFiles();
+
+console.log(output);
